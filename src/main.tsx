26b80b6977d2181466f12216c2a7e73b4d28973c
Alex <stark.11raj@gmail.com>
2025-06-16 15:45:51 +0300

    Small fixes

--- src/main.tsx
+++ src/main.tsx
@@ -1,15 +1,11 @@
 //Main
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import { HeroUIProvider } from '@heroui/react'
-import React from 'react';
-import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 
 import RouterWrapper from './RouterWrapper.jsx';
 
 import './index.css'
-import App from './App.tsx'
 
 createRoot(document.getElementById('root')!).render(
   
